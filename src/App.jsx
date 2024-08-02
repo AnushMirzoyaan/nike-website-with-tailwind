eaa503c0532b2818531477086443fce9d08acec9
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2024-08-02 17:15:54 +0400

    Adding application structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
 export default function App() {
   return (
-    <section>
-      nav
-      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-      <section className="padding">Popular products</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Servecies</section>
-      <section className="padding">SpecialOffer</section>
-      <section className="bg-pale-blue padding">CustomerReviews</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className=" bg-black padding-x padding-t pb-8">Footer</section>
-    </section>
+    <main className="relative">
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className=" bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
   );
 }
