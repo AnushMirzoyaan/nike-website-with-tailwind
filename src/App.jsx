d9db2e8ef1add3cfe2d13ebc387ed436089cce18
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2024-08-02 17:02:32 +0400

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <section>
+      nav
+      <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
+      <section className="padding">Popular products</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Servecies</section>
+      <section className="padding">SpecialOffer</section>
+      <section className="bg-pale-blue padding">CustomerReviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className=" bg-black padding-x padding-t pb-8">Footer</section>
+    </section>
+  );
+}
