81ad4e54285b272379b2a7fcdce6a6b7709acbe8
AnushMirzoyaan <anush.mirzoyan1999@gmail.com>
2024-08-05 15:07:20 +0400

    Creating Reviews section

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -7,7 +7,7 @@ const PopularProductCard = ({ imageURL, name, price }) => {
 
       <div className="mt-8 flex justify-start gap-2.5">
         <img src={star} alt="rating" width={24} height={24} />
-        <p className="font-montserrat text-lg leading-normal text-slate-gray">
+        <p className="font-montserrat text-xl leading-normal text-slate-gray">
           (4.5)
         </p>
       </div>
